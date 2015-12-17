0e67abd704b3a040f0a8584d3022fc6da969453f
EnumCoder <EnumCoder@gmail.com>
2015-12-17 17:35:14 -0500

    Progress done so far.

---
+++ src/messagedirector/MDParticipantInterface.py
@@ -0,0 +1,13 @@
+from src.messagedirector.MDParticipant import MDParticipant
+
+class MDParticipantInterface:
+	
+	def __init__(self):
+		self.registeredParticipants = {}
+		self.potentialParticipants = []
+	
+	def register_channel(self, connection, channel):
+		pass
+	
+	def unregister_channel(self, connection, channel):
+		pass
